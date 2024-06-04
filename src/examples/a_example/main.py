828bf59496b4ed3a9b5f2ad46b734b0755400452
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-06-04 00:36:31 +0000

    input w numerical values

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,7 +1,16 @@
 import devprocess
 
-result = devprocess.subtract_numbers(10, 5)
+value1 = input("Enter a number: ") #python treats numbers as characters from input
+value2 = input("Enter a number: ")
 
+result = devprocess.add_numbers(int(value1), int(value2)) #convert the value to an integer using the int function
+
+print(result)
+
+value1 = input("Enter a number: ") #python treats numbers as characters from input
+value2 = input("Enter a number: ")
+
+result = devprocess.subtract_numbers(float(value1), float(value2))
 print(result)
 
 
