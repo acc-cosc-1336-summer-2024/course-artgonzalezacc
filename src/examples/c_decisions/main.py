06cc8fd2a745cefd6c82959922f835c72106635a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-06-06 02:14:49 +0000

    into to if elif

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -1,14 +1,8 @@
 import decisions
 
 value1 = input("Enter a number: ") #read form keyboard , Python brings in values as a string variable
-#check if is a number
 
-result = decisions.is_number_not_in_range(int(value1), 1, 10) #convert value to an integer number with the int function
+generation = decisions.get_generation(int(value1))
 
-if(result == True):
-    print(value1, " not in range")
-else:
-    print(value1, " in range")
-
-print(result)
+print(generation)
 
--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.c_decisions.decisions import is_number_in_range, is_number_not_in_range, test_config
+from src.examples.c_decisions.decisions import get_generation, is_number_in_range, is_number_not_in_range, test_config
 
 class Test_Config(unittest.TestCase):
 
@@ -16,4 +16,15 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(True, is_number_not_in_range(0, 1, 10))
         self.assertEqual(True, is_number_not_in_range(11, 1, 10))
         self.assertEqual(False, is_number_not_in_range(5, 1, 10))
+
+    def test_get_generation(self):
+        
+        self.assertEqual('Invalid Option', get_generation(1900))
+        self.assertEqual('Centennial', get_generation(2000))
+        self.assertEqual('Millenial', get_generation(1990))
+        self.assertEqual('Generation X', get_generation(1970))
+        self.assertEqual('Baby Boomer', get_generation(1960))
+        self.assertEqual('Silent Generation', get_generation(1930))
+        self.assertEqual('Invalid Option', get_generation(2025))
+
 
