8baeba46a4ec41399e53e27bebbcbe7504a8ad8c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-06-12 23:56:03 +0000

    void and value return functions local variables

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,1 +1,8 @@
 #main program
+import void_functions
+
+void_functions.local_variables(5, 8) #function arguments --. feed in to the function parameters
+
+print('value return function....')
+result = void_functions.local_variables_value_return(10, 15)
+print(result)
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -1,1 +1,16 @@
 #
+#print(var1) can't use outside of the function
+
+def local_variables(num1, num2):
+    var1 = 5
+    var2 = "Python"
+
+    print(num1, num2, var1, var2)
+
+
+#print(var1) can't use outside of the function
+
+def local_variables_value_return(num1, num2):
+    num3 = num1 + num2
+
+    return num3
