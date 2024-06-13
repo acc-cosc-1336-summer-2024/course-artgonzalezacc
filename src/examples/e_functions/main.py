2e63f7062b74aea96146d9160e28bfaab9c0e357
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-06-13 00:19:12 +0000

    using global variables in a function

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,9 +1,13 @@
 #main program
 import void_functions
 
+num1 = 100 #global variable
+num2 = 200 #global variable
 
 def main():
+    global num1 #tell python to use num1 global variable
     num1 = 2 #local variables - belong to main 
+    
     num2 = 3  #local variables - belong to main 
     void_functions.local_variables(5, 8) #function arguments --. feed in to the function parameters
     print('After local_variables executes')
@@ -14,3 +18,6 @@ def main():
     print(result)
 
 main()
+
+#display global variable
+print(num1, num2)
