2cc7e7d321339ce7e19d9ca87625633e66b549fa
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-06-13 00:23:39 +0000

    global constant honor system

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,13 +1,14 @@
 #main program
 import void_functions
 
+FEDERAL_TAX_RATE = .12 #global constant
 num1 = 100 #global variable
 num2 = 200 #global variable
 
 def main():
     global num1 #tell python to use num1 global variable
     num1 = 2 #local variables - belong to main 
-    
+
     num2 = 3  #local variables - belong to main 
     void_functions.local_variables(5, 8) #function arguments --. feed in to the function parameters
     print('After local_variables executes')
@@ -16,6 +17,8 @@ def main():
     print('value return function....')
     result = void_functions.local_variables_value_return(10, 15)
     print(result)
+    
+    print(FEDERAL_TAX_RATE)
 
 main()
 
