ab117f609981f39920e76c25c770ceb60c4eac67
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-06-17 23:59:12 +0000

    random num generator functions

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,26 +1,9 @@
 #main program
-import void_functions
-
-FEDERAL_TAX_RATE = .12 #global constant
-num1 = 100 #global variable
-num2 = 200 #global variable
+import value_return_functions
 
 def main():
-    global num1 #tell python to use num1 global variable
-    num1 = 2 #local variables - belong to main 
-
-    num2 = 3  #local variables - belong to main 
-    void_functions.local_variables(5, 8) #function arguments --. feed in to the function parameters
-    print('After local_variables executes')
-    print(num1, num2)
-
-    print('value return function....')
-    result = void_functions.local_variables_value_return(10, 15)
-    print(result)
-    
-    print(FEDERAL_TAX_RATE)
+    for i in range(0, 20):
+        print(value_return_functions.get_random_int(1, 20))
 
 main()
 
-#display global variable
-print(num1, num2)
--- src/examples/e_functions/value_return_functions.py
+++ src/examples/e_functions/value_return_functions.py
@@ -1,2 +1,9 @@
+import random #python library
+
 def test_config():
     return True
+
+def get_random_int(min, max):
+    number = random.randint(min, max)
+
+    return number
