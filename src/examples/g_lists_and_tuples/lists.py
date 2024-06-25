7e847c173f050790813e492c6a6186122f1285a6
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-06-25 00:04:00 +0000

    list support different data types as items

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -12,3 +12,36 @@ def create_array():
     for n in range(0, len(nums)):
         print(nums[n])
 
+def create_list():
+    nums = [2, 4, 6, 8, 10]
+
+    print(nums[0])
+    print(nums[1])
+    print("length of the list", len(nums))
+
+    print("iterate w a loop")
+
+    for i in range(0, len(nums)):
+        print(nums[i])
+
+def loop_list_w_while():
+    nums = [2, 4, 6, 8, 10]
+    indx = 0
+
+    while(indx < len(nums)):
+        print(nums[indx])
+        indx += 1
+
+def use_different_data_types_in_a_list():
+    items = [1, 2.5, True, "python"]
+
+    for i in items: #fastest way to code- iterate a list
+        print(i)
+
+    print(items)
+
+
+
+
+
+
--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,4 +1,6 @@
 #main program
 import lists
 
-lists.create_array()
+#lists.create_list()
+#lists.loop_list_w_while()
+lists.use_different_data_types_in_a_list()
