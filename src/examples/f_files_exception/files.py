cbde0968f3e68d02394f6b06fd2d30f45fb70e9f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-03 23:30:59 +0000

    create, write, read file

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,26 @@
 #
+def open_file(file_name):
+    file = open(file_name, 'r')
+
+    #nothing
+
+    file.close()
+
+def write_to_file(file_name):
+    file = open(file_name, 'w')
+
+    file.write('John Locke\n')
+    file.write('David Hume\n')
+    file.write('Edmund Burke\n')
+
+    file.close()
+
+def read_from_file(file_name):
+    file = open(file_name, 'r')
+
+    files_lines = file.read()
+
+    print(files_lines)
+
+    file.close()
+
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,1 +1,8 @@
 #main program
+import files
+
+file_name = 'philosophers.txt'
+
+#files.open_file('philosophers.txt')
+#files.write_to_file(file_name)
+files.read_from_file(file_name)
