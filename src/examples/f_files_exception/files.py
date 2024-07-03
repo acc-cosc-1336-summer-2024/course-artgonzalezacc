0de8683898d4a93905d9344a97436b857bca70b5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-03 23:33:57 +0000

    manually read lines from file

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -24,3 +24,15 @@ def read_from_file(file_name):
 
     file.close()
 
+def read_from_file_one_line_at_a_time(file_name):
+    file = open(file_name, 'r')
+
+    line1 = file.readline().rstrip('\n')
+    line2 = file.readline().rstrip('\n')
+    line3 = file.readline().rstrip('\n')
+
+    print(line1)
+    print(line2)
+    print(line3)
+
+    file.close()
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -5,4 +5,5 @@ file_name = 'philosophers.txt'
 
 #files.open_file('philosophers.txt')
 #files.write_to_file(file_name)
-files.read_from_file(file_name)
+#files.read_from_file(file_name)
+files.read_from_file_one_line_at_a_time(file_name)
