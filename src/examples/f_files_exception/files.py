da646a4253ae33f2f078af14dfdcbebbb8a2ddfd
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-04 00:10:37 +0000

    read w loops, writing records to file

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -36,3 +36,110 @@ def read_from_file_one_line_at_a_time(file_name):
     print(line3)
 
     file.close()
+
+def append_to_file(file_name):
+    file = open(file_name, 'a')
+
+    file.write('Carl Marx\n')
+
+    file.close()
+
+def write_sales_data(file_name):
+    file = open(file_name, 'w')
+
+    num1 = input("Enter sales data: ")
+    num2 = input("Enter sales data: ")
+    num3 = input("Enter sales data: ")
+
+    file.write(num1 + '\n')
+    file.write(num2 + '\n')
+    file.write(num3 + '\n')
+    
+    file.close()
+
+def read_sales_data(file_name):
+    file = open(file_name, 'r')
+
+    num1 = int(file.readline())
+    num2 = int(file.readline())
+    num3 = int(file.readline())
+
+    total = num1 + num2 + num3
+
+    print(f'The numbers are: {num1} {num2} {num3}')
+    print(f'Their total is: {total}')
+    
+    file.close()
+
+def read_sales_data_w_while_loop(file_name):
+    file = open(file_name, 'r')
+
+    line = file.readline()
+
+    total = 0
+
+    while line != '':
+        amount = int(line)
+        total += amount
+        print(amount)
+
+        line = file.readline()
+        
+    print()
+    print(total)
+
+    file.close()
+
+def read_sales_data_w_for_loop(file_name):
+    file = open(file_name, 'r')
+
+    total = 0
+
+    for line in file:
+        amount = int(line)
+        total += amount
+        print(amount)
+
+    print()
+    print(total)
+
+    file.close()
+
+def write_employee_records(file_name):
+    file = open(file_name, 'w')
+
+    choice = '1'
+
+    while choice == '1':
+        id = input('Enter id:')
+        name = input('Enter name: ')
+        dept = input('Enter dept: ')
+
+        file.write(id + '\t')
+        file.write(name + '\t')
+        file.write(dept + '\n')
+
+        choice = input('Enter 1 to continue')
+
+
+    file.close()
+
+def read_employee_records(file_name):
+    file = open(file_name, 'r')
+
+    for line in file:
+        record = line.split('\t') #creates a list in this format ['1', 'joe', 'dept']
+        id = record[0]
+        name = record[1]
+        dept = record[2].rstrip('\n')
+
+        print(id, name, dept)
+
+    file.close()
+        
+
+        
+        
+
+
+
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,9 +1,7 @@
 #main program
 import files
 
-file_name = 'philosophers.txt'
+file_name = 'employees.txt'
 
-#files.open_file('philosophers.txt')
-#files.write_to_file(file_name)
-#files.read_from_file(file_name)
-files.read_from_file_one_line_at_a_time(file_name)
+#files.write_employee_records(file_name)
+files.read_employee_records(file_name)
