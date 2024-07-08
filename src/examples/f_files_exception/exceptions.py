b27a19bcdcccc07cf2ad30746bc514e0ea325ad5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-08 23:29:30 +0000

    multiply two numbers no validation

--- src/examples/f_files_exception/exceptions.py
+++ src/examples/f_files_exception/exceptions.py
@@ -11,3 +11,11 @@ def divide_two_numbers(num1, num2):
         return "Division by 0 not allowed"
 
     return result
+
+def multiply_two_numbers():
+    
+    num1 = int(input("Enter num1: "))
+    num2 = float(input("Enter num2: "))
+
+    result = num1 * num2
+    print(result)
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,7 +1,5 @@
 #main program
 import exceptions
 
-result = exceptions.divide_two_numbers(2, 4)
-print(result)
-
+exceptions.multiply_two_numbers()
 
