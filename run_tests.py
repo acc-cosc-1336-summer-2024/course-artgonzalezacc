2a14b2da179801c96ceddb8a947b1adc90fb87ab
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-09 00:09:55 +0000

    use -- expect  exceptions in test cases

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
 #from tests.homework.b_in_proc_out import tests_in_proc_out
-from tests.examples.i_dictionaries_sets import tests_dictionaries_and_sets
+from tests.examples.f_files_exception import tests_files_exception
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
+suite = unittest.TestLoader().loadTestsFromModule(tests_files_exception)
 unittest.TextTestRunner(verbosity=2).run(suite)
