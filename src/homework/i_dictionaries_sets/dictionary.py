7512ead1e675d95e206a7a80b041de5ec36cfa6b
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-10 23:24:03 +0000

    save survey responses

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -9,8 +9,11 @@ survey_questions_dictionary = \
 }
 
 survey_response_options = {1:'Never',2:'Sometimes',3:'More',4:'More more',5:'Always', 6:'Always Always'}
+survey_responses_list = [] #surveyid, questionid, response
 
 def display_survey_question():
+
+    survey_id = 1
 
     for question_id, question in survey_questions_dictionary.items():
         print(question)
@@ -18,4 +21,12 @@ def display_survey_question():
         for option, value in survey_response_options.items():
             print(option, value)
 
-        response = input("Enter option: ")
+        response = int(input("Enter option: "))
+
+        capture_survey_response(survey_id, question_id, response)
+        survey_id += 1
+        
+        print(survey_responses_list)
+
+def capture_survey_response(survey_id, question_id, response):
+    survey_responses_list.append([survey_id, question_id, response])
