542ee4c2730b2fc462ad55346dd7a0f7d21e57f9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-16 00:26:59 +0000

    add deposit

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -2,6 +2,13 @@ import account
 
 a = account.Account(250) #created a variable/object of the Account class type
 balance = a.get_balance()
+print(balance)
+
+
+amount = int(input("enter deposit amount: "))
+
+a.deposit(amount)
+balance = a.get_balance()
 
 print(balance)
 
