e40b9ea620bc19298a76a66e23a60b603ad72626
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-18 17:50:52 +0000

    to eliminate list retrieval issue

--- src/examples/j_classes/customer.py
+++ src/examples/j_classes/customer.py
@@ -1,14 +1,15 @@
 from src.examples.j_classes.account import Account
 
 class Customer:
-    __list_accounts  = []
 
     def __init__(self, checking_balance, savings_balance):
+        self.__list_accounts  = []    
         checking_account = Account(checking_balance) 
         savings_account = Account(savings_balance)
 
         self.__list_accounts.append(checking_account)
         self.__list_accounts.append(savings_account)
+        print(self.__list_accounts[0].get_balance(), self.__list_accounts[1].get_balance())
 
     def get_account(self, index):
         return self.__list_accounts[index]
