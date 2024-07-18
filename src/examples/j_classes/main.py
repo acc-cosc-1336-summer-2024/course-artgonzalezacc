dbe7c6e8f7fc5a60b70059ccc74403e4246c59da
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-07-18 00:19:03 +0000

    atm menu

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -1,18 +1,7 @@
-from src.examples.j_classes.atm import ATM
 from src.examples.j_classes.customer import Customer
+from src.examples.j_classes.menu import run_menu
 
 customer1 = Customer(100, 50)
-
-a = customer1.get_account(0)
-balance = a.get_balance()
-print(balance)
-
-atm = ATM(a)
-atm.make_deposit()
-
-atm.display_balance()
-
-atm.make_withdraw()
-atm.display_balance()
+run_menu(customer1)
 
 
---
+++ src/examples/j_classes/menu.py
@@ -0,0 +1,34 @@
+from src.examples.j_classes.atm import ATM
+
+def display_menu():
+    print('1-Display Balance')
+    print('2-Deposit')
+    print('3-Withdraw')
+    print('4-Exit')
+
+def run_menu(customer):
+
+    account_index = int(input("Enter 1 for checking 2 for savings: "))
+    account = customer.get_account(account_index - 1)
+
+    atm = ATM(account)
+
+    choice = -1
+
+    while choice != 4:
+        display_menu()
+        choice = int(input('Enter menu option: '))
+        handle_menu_option(choice, atm)
+
+def handle_menu_option(choice, atm):
+    if(choice == 1):
+        atm.display_balance()
+    elif(choice == 2):
+        atm.make_deposit()
+    elif(choice == 3):
+        atm.make_withdraw()
+    elif(choice == 4):
+        print('Exiting...')
+    else:
+        print('Invalid option...')
+
